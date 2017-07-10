37bb5b9d29a4e48d4c4a12bb963ea9b759cb8b22
PNPtutorials <parvinderyadav95@gmail.com>
2017-07-10 19:14:37 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+echo "**********************Performing update************************"
+apt-get update
+echo "**********************Installing Vino************************"
+apt-get -y install vino
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+
+echo "VNC server setup done"
+mkdir /root/.config
+cd /root/.config
+mkdir autostart
+
+echo "**********************Making autorun VNC script************************"
+echo "[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=VINO
+Comment=
+Exec=/usr/lib/vino/vino-server
+StartupNotify=false
+Terminal=false
+Hidden=false" > /root/.config/autostart/vino.desktop
+
+echo "VNC autorun on startup done"
+
+echo "****************Enabling autologin***************"
+
+#mv /etc/lightdm/lightdm.conf  /etc/lightdm/lightdm.conf.bak
+#mv /etc/pam.d/lightdm-autologin  /etc/pam.d/lightdm-autologin.bak
+#cp lightdm.conf /etc/lightdm/lightdm.conf
+#cp lightdm-autologin /etc/pam.d/lightdm-autologin
