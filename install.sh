d572e41bf0e0f738ef9fb2883ae0b35687e24648
PNPtutorials <parvinderyadav95@gmail.com>
2017-08-22 11:50:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,3 +33,8 @@ mv /etc/lightdm/lightdm.conf  /etc/lightdm/lightdm.conf.bak
 mv /etc/pam.d/lightdm-autologin  /etc/pam.d/lightdm-autologin.bak
 cp lightdm.conf /etc/lightdm/lightdm.conf
 cp lightdm-autologin /etc/pam.d/lightdm-autologin
+
+echo "setting up interafces file"
+
+mv /etc/network/interfaces /etc/network/interfaces.bak
+mv interfaces /etc/network/interfaces
