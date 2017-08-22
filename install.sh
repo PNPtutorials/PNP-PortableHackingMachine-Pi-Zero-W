9b8102829643bce9aac5c124fa7ba0515a4565d2
PNPtutorials <parvinderyadav95@gmail.com>
2017-08-22 12:50:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ echo "setting up interafces file"
 mv /etc/network/interfaces /etc/network/interfaces.bak
 echo " "
 echo "Your Current network configuration backed up as /etc/network/interfaces.bak file"
-mv interfaces /etc/network/interfaces
+cp interfaces /etc/network/interfaces
 echo "END of Script... Thanks for using it"
 echo " "
 echo "Please Subscribe: https://YouTube.com/PNPtutorials"
